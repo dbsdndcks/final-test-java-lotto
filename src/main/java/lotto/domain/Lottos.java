3b3b7e0fd281909edeffb12f38afe5a70b0c6d22
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 01:14:09 +0900

    feat: 로또 리스트 문자열 변환 기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = new ArrayList<>(lottos);
+    }
+
+    public String formatString() {
+        return lottos.stream()
+                .map(Lotto::toString)
+                .collect(Collectors.joining("\n"));
+    }
 }
