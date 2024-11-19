ca4af6d19f7546a44d6e6f8efeaa826aa04b6bc3
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-19 15:39:56 +0900

    feat: 도메인 설계

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,6 +1,26 @@
 package lotto.config;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class AppConfig {
+    public LottoController lottoController() {
+        return  new LottoController(InputView(), outputView(), lottoService());
+    }
+
+    private InputView InputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private LottoService lottoService() {
+        return new LottoService();
+    }
 
 
 }
