bc41ad1f357304babb76f0fbf973d8a9bfd900b3
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-19 17:13:37 +0900

    feat: 랜덤값 생성 기능 추가

--- src/main/java/lotto/util/random/RandomGenerater.java
+++ src/main/java/lotto/util/random/RandomGenerater.java
@@ -1,4 +1,11 @@
 package lotto.util.random;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class RandomGenerater {
+
+    public static List<Integer> randomLottoGenerater() {
+        return Randoms.pickUniqueNumbersInRange(1,45,6);
+    }
 }
