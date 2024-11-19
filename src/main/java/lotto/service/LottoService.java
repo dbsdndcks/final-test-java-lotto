8ea1dc3e9262e7ea166bf0f3abebf59eda3354fd
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 01:15:05 +0900

    feaet: factory클래스에서 도메인 생성기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,17 @@
 package lotto.service;
 
+import lotto.domain.LottoFactory;
+import lotto.domain.Lottos;
+
 public class LottoService {
+
+    private LottoFactory lottoFactory;
+
+    public LottoService(LottoFactory lottoFactory) {
+        this.lottoFactory = lottoFactory;
+    }
+
+    public Lottos createLottos(int money) {
+        return lottoFactory.createLottos(money);
+    }
 }
