f18a85dfa76a3283ac2e6955f27668f2cf516f59
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-19 17:13:24 +0900

    feat: 입력 검증 기능추가

--- src/main/java/lotto/util/validate/InputValidater.java
+++ src/main/java/lotto/util/validate/InputValidater.java
@@ -1,4 +1,27 @@
 package lotto.util.validate;
 
 public class InputValidater {
+    public static void validateInputMoney(String purchaseMoney) {
+        validateEmpty(purchaseMoney;
+        validateNumeric(purchaseMoney);
+        validateUnitOfMoney(purchaseMoney);
+    }
+
+    private static void validateUnitOfMoney(String purchaseMoney) {
+        if (purchaseMoney.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateNumeric(String purchaseMoney) {
+        if (purchaseMoney.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateEmpty(String purchaseMoney) {
+        if (Integer.parseInt(purchaseMoney) % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
