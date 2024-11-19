2f2fcd845abb95c7782c8ade0825c07581098c7f
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 01:13:44 +0900

    refactor: 입력값 숫자 검증 패턴으로 변환

--- src/main/java/lotto/util/validate/InputValidater.java
+++ src/main/java/lotto/util/validate/InputValidater.java
@@ -1,26 +1,30 @@
 package lotto.util.validate;
 
+
 public class InputValidater {
+
+    private final static String NUMERIC_PATTERN = "^[0-9]*$";
+
     public static void validateInputMoney(String purchaseMoney) {
-        validateEmpty(purchaseMoney;
+        validateEmpty(purchaseMoney);
         validateNumeric(purchaseMoney);
         validateUnitOfMoney(purchaseMoney);
     }
 
-    private static void validateUnitOfMoney(String purchaseMoney) {
+    private static void validateEmpty(String purchaseMoney) {
         if (purchaseMoney.isEmpty()) {
             throw new IllegalArgumentException();
         }
     }
-
     private static void validateNumeric(String purchaseMoney) {
-        if (purchaseMoney.chars().allMatch(Character::isDigit)) {
+        if (!purchaseMoney.matches(NUMERIC_PATTERN)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void validateEmpty(String purchaseMoney) {
-        if (Integer.parseInt(purchaseMoney) % 1000 != 0) {
+    private static void validateUnitOfMoney(String purchaseMoney) {
+        int money = Integer.parseInt(purchaseMoney);
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException();
         }
     }
